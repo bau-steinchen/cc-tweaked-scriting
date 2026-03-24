9a0d1ea1c33b812eae5a25b13846933945e4ecf4
Mikko Ristein <mikko.ristein@plath-signalproducts.com>
2026-03-24 09:53:19 +0100

    ad more position values and small adaptions

--- depo/depo-monitor.lua
+++ depo/depo-monitor.lua
@@ -11,31 +11,40 @@ mon.clear()
 -------------------------------------------------
 
 local Nodes = {
-    checkpoint  = { row = 2, textX = 22, leftX = 9,  rightX = 31, name = "Depo" },
-    depo1       = { row = 3, textX = 22, leftX = 9,  rightX = 31, name = "Depo" },
-    depo2       = { row = 4, textX = 22, leftX = 9,  rightX = 31, name = "Depo" },
-    depo3       = { row = 5, textX = 22, leftX = 9,  rightX = 31, name = "Depo" },
-    depo4       = { row = 6, textX = 22, leftX = 9,  rightX = 31, name = "Depo" },
-    depo5       = { row = 7, textX = 22, leftX = 9,  rightX = 31, name = "Depo" },
-    depo6       = { row = 8, textX = 22, leftX = 9,  rightX = 31, name = "Depo" },
-    depo7       = { row = 9, textX = 22, leftX = 9,  rightX = 31, name = "Depo" },
-    depo8       = { row = 10, textX = 22, leftX = 9,  rightX = 31, name = "Depo" },
-    depo9       = { row = 11, textX = 22, leftX = 9,  rightX = 31, name = "Depo" },
-    depo10      = { row = 12, textX = 22, leftX = 9,  rightX = 31, name = "Depo" },
-    depo11      = { row = 13, textX = 22, leftX = 9,  rightX = 31, name = "Depo" },
+    Checkpoint  = { row = 13, textX = 38,  name = "computer_14", text="Checkpoint", type="Station", signal="Create_Signal_4", offset= 12},
+    depo1       = { row = 12, textX = 45,  name = "computer_13", text="Depo",       type="Station", signal="Create_Signal_5", offset= 6},
+    depo2       = { row = 11, textX = 45,  name = "computer_12", text="Depo",       type="Station", signal="Create_Signal_6", offset= 6},
+    depo3       = { row = 10, textX = 45,  name = "computer_11", text="Depo",       type="Station", signal="Create_Signal_7", offset= 6},
+    depo4       = { row = 9 , textX = 45,  name = "computer_10", text="Depo",       type="Station", signal="Create_Signal_8", offset= 6},
+    depo5       = { row = 8 , textX = 45,  name = "computer_9" , text="Depo",       type="Station", signal="Create_Signal_9", offset= 6},
+    depo6       = { row = 7 , textX = 45,  name = "computer_8" , text="Depo",       type="Station", signal="Create_Signal_10", offset= 6},
+    depo7       = { row = 6 , textX = 45,  name = "computer_7" , text="Depo",       type="Station", signal="Create_Signal_11", offset= 6},
+    depo8       = { row = 5 , textX = 45,  name = "computer_6" , text="Depo",       type="Station", signal="Create_Signal_12", offset= 6},
+    depo9       = { row = 4 , textX = 45,  name = "computer_5" , text="Depo",       type="Station", signal="Create_Signal_13", offset= 6},
+    depo10      = { row = 3 , textX = 45,  name = "computer_4" , text="Depo",       type="Station", signal="Create_Signal_14", offset= 6},
+    depo11      = { row = 2 , textX = 45,  name = "computer_3" , text="Depo",       type="Station", signal="Create_Signal_15", offset= 6},
     -- Signals
-    entry1      = { row = 21, textX = 6,  name = "computer_51", text=" ", type="signal"},
-    entry2      = { row = 16, textX = 9,  name = "computer_46", text=" ", type="signal"},
-    entry3      = { row = 19, textX = 16, name = "computer_47", text=" ", type="signal"},
-    entry4      = { row = 17, textX = 14, leftX = 29, rightX = 11, name = "Signal"},
-    entry5      = { row = 9, textX = 14, leftX = 29, rightX = 11, name = "Signal"},
-    entry6      = { row = 9, textX = 14, leftX = 29, rightX = 11, name = "Signal"},
-    entry7      = { row = 9, textX = 14, leftX = 29, rightX = 11, name = "Signal"},
-    entry8      = { row = 9, textX = 14, leftX = 29, rightX = 11, name = "Signal"},
-    entry9      = { row = 9, textX = 14, leftX = 29, rightX = 11, name = "Signal"},
-    entry10     = { row = 9, textX = 14, leftX = 29, rightX = 11, name = "Signal"},
-    entry11     = { row = 9, textX = 14, leftX = 29, rightX = 11, name = "Signal"},
-    entry12     = { row = 9, textX = 14, leftX = 29, rightX = 11, name = "Signal"},
+    entry1      = { row = 21, textX = 6,   name = "computer_51", text=" ", type="signal", signal="Create_Signal_18"},
+    entry2      = { row = 16, textX = 9,   name = "computer_46", text=" ", type="signal", signal="top"},
+    entry3      = { row = 19, textX = 16,  name = "computer_47", text=" ", type="signal", signal="Create_Signal_3"},
+    entry4      = { row = 13, textX = 14,  name = "computer_20", text=" ", type="signal", signal="top"},
+    entry5      = { row = 12, textX = 14,  name = "computer_21", text=" ", type="signal", signal="top"},
+    entry6      = { row = 11, textX = 14,  name = "computer_22", text=" ", type="signal", signal="top"},
+    entry7      = { row = 10, textX = 14,  name = "computer_23", text=" ", type="signal", signal="top"},
+    entry8      = { row = 9 , textX = 14,  name = "computer_24", text=" ", type="signal", signal="top"},
+    entry9      = { row = 8 , textX = 14,  name = "computer_25", text=" ", type="signal", signal="top"},
+    entry10     = { row = 7 , textX = 14,  name = "computer_26", text=" ", type="signal", signal="top"},
+    entry11     = { row = 6 , textX = 14,  name = "computer_27", text=" ", type="signal", signal="top"},
+    entry12     = { row = 5 , textX = 14,  name = "computer_28", text=" ", type="signal", signal="top"},
+    entry13     = { row = 4 , textX = 14,  name = "computer_29", text=" ", type="signal", signal="top"},
+    entry14     = { row = 3 , textX = 14,  name = "computer_30", text=" ", type="signal", signal="top"},
+    entry15     = { row = 2 , textX = 14,  name = "computer_31", text=" ", type="signal", signal="top"},
+    entry16     = { row = 16, textX = 58,  name = "computer_17", text=" ", type="signal", signal="top"},
+    entry17     = { row = 17, textX = 63,  name = "computer_18", text=" ", type="signal", signal="top"},
+    entry18     = { row = 19, textX = 54,  name = "computer_19", text=" ", type="signal", signal="Create_Signal_"},
+    entry19     = { row = 17, textX = 54,  name = "computer_", text=" ", type="signal", signal="Create_Signal_"},
+    entry20     = { row = 19, textX = 63,  name = "computer_", text=" ", type="signal", signal="Create_Signal_"},
+    entry21     = { row = 2 , textX = 14,  name = "computer_", text=" ", type="signal", signal="top"},
 }
 
 -------------------------------------------------
@@ -100,6 +109,10 @@ local function drawNode(node, color)
         mon.setTextColor(color)
         mon.write(node.text)
         mon.setTextColor(colors.white)
+        mon.setCursorPos(node.textX + node.offset, node.row)
+        mon.setBackgroundColor(color)
+        mon.write(" ")
+        mon.setBackgroundColor(colors.black)
     end
 
 end
